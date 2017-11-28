fd33048ed3864bce382d88be5be728a9fc5808c8
ewolfe <e@ewolfe.me>
2017-11-27 17:43:06 -0800

    feat: implement proof of concept (#7)
    * Setup simple http server
    
    * Setup end-to-end flow
    
    * Polish readme

--- README.md
+++ README.md
@@ -1,11 +1,86 @@
 <p align="center">
-	<img width="248" src="./assets/logomark-stylized.svg" alt="PRLint">
+	<img width="248" src="https://cdn.rawgit.com/ewolfe/prlint/90e46d36/assets/logomark-stylized.svg" alt="PRLint">
 </p>
 
 # PRLint
 
 > GitHub App for linting pull requests
 
-###### Credit
+- Lint pull request branch name
+- Lint pull request title
+- Lint pull request description
+- Lint all the things...
+- Anything listed in the [pull request object](https://developer.github.com/v3/pulls/#get-a-single-pull-request
+)
+
+## Install
+
+[https://github.com/apps/prlint](https://github.com/apps/prlint)
+
+[![Install](https://cdn.rawgit.com/ewolfe/prlint/90e46d36/assets/install-screenshot.png)](https://github.com/apps/prlint)
+
+## Usage
+
+1. Add this file `.github/prlint.json` to the root of your project:
+
+	```json
+{
+	"title": [
+	  {
+	    "pattern": "^hello world",
+	    "flags": ["i"]
+	  }
+	]
+}
+	```
+1. Then open a pull request with the title "hello world" to get a successful commit status. You can also set the title to "HELLO WORLD" since we passed the flag `i` which makes our regex case insensitive.
+
+## API
+
+### Keys:
+
+The top level keys are keys that the GitHub API exposes when a pull request is opened, edited, etc.
+
+You can use anything listed in the sample response object here https://developer.github.com/v3/pulls/#get-a-single-pull-request
+
+To target a nested object, you can use dot notation encoded within the key string. ie:
+
+```json
+{
+  "milestone.id": [
+    {
+      "pattern": "\d"
+    }
+  ]
+}
+```
+
+### Values:
+
+The top level values ire where you get to define your validation rules. You can have multiples rules, so we expect an array (even if you only have a single validation rule).
+
+- Each item in the array needs to be an object.
+- Each object requires a `pattern` key with a string value
+- The `flags` array is optional
+
+> See [RegExp Syntax](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp#Syntax) for full documentation on MDN.
+
+
+## Support
+
+If you’re reading this, congratulations! You’re one of the first ~250 users. For any bugs or feature requests please send me a direct email at [e@ewolfe.me](mailto:e@ewolfe.me) or open an [issue](https://github.com/ewolfe/prlint/issues/new).
+
+(Why 250? GitHub apps need a [minimum of 250 users](https://developer.github.com/apps/adding-integrations/listing-apps-on-github-marketplace/requirements-for-listing-an-app-on-github-marketplace/) before I can list it on the marketplace. And until then I’m okay with getting direct emails for support.)
+
+## Developing
+
+1. Get the `.env` file from @ewolfe
+1. `$ nvm use`
+1. `$ yarn`
+1. `$ yarn dev`
+
+---
+
+###### Credits
 
 - [“Pinch”](https://thenounproject.com/term/pinch/736992/) icon by Anna Ho from [the Noun Project](https://thenounproject.com/).
---
+++ index.js
@@ -0,0 +1,137 @@
+const flatten = require('flat');
+const fs = require('fs');
+const got = require('got');
+const jsonwebtoken = require('jsonwebtoken');
+const { json, send } = require('micro');
+
+function newJsonWebToken() {
+  // https://developer.github.com/apps/building-integrations/setting-up-and-registering-github-apps/about-authentication-options-for-github-apps/#authenticating-as-a-github-app
+  const payload = {
+    iat: Math.floor(Date.now() / 1000),
+    exp: Math.floor(Date.now() / 1000) + (10 * 60),
+    iss: 7012 // https://github.com/settings/apps/prlint
+  }
+
+  let privateKeyBuffer = new Buffer(process.env.PRIVATE_KEY_B64, 'base64');
+  let privateKey = privateKeyBuffer.toString('ascii');
+  return jsonwebtoken.sign(payload, privateKey, { algorithm: 'RS256'});
+}
+
+async function updateShaStatus(body, res) {
+  const pull_request_flat = flatten(body.pull_request);
+  const accessToken = accessTokens[`${body.installation.id}`].token;
+
+  try {
+    const configUrl = `https://api.github.com/repos/${body.repository.full_name}/contents/.github/prlint.json?ref=${body.pull_request.head.ref}`
+    const config = await got(configUrl, {
+      headers: {
+        Accept: "application/vnd.github.machine-man-preview+json",
+        Authorization: `token ${accessToken}`
+      }
+    });
+
+    const userConfigBase64 = JSON.parse(config.body).content
+    const userConfig = JSON.parse(Buffer.from(userConfigBase64, 'base64'));
+
+    let failureMessages = [];
+    Object.keys(userConfig).map(element => {
+      userConfig[element].forEach((item, index) => {
+        const pattern = item.pattern;
+        const regex = new RegExp(pattern, item.flags || '');
+        pass = regex.test(pull_request_flat[element])
+        if (!pass) {
+          failureMessages.push(`\`/${item.pattern}/.test('${pull_request_flat[element]}')\` failed`);
+        }
+      })
+    })
+
+    let bodyPayload = {};
+    if (!failureMessages.length) {
+      bodyPayload = {
+        state: 'success',
+        description: 'Your validation rules passed',
+        context: 'PRLint'
+      };
+    } else {
+      bodyPayload = {
+        state: 'failure',
+        description: failureMessages[0],
+        target_url: `https://github.com/${body.repository.full_name}/blob/${body.pull_request.head.sha}/.github/prlint.json`,
+        context: 'PRLint'
+      }
+    }
+
+    try {
+      const statusUrl = `https://api.github.com/repos/${body.repository.full_name}/statuses/${body.pull_request.head.sha}`
+      const status = await got.post(statusUrl, {
+        headers: {
+          Accept: "application/vnd.github.machine-man-preview+json",
+          Authorization: `token ${accessToken}`
+        },
+        body: bodyPayload,
+        json: true
+      });
+      send(res, 200, bodyPayload);
+    } catch (exception) {
+      send(res, 500, exception);
+    }
+  } catch (exception) {
+    let description = exception.toString();
+    if (exception.response.statusCode === 404) {
+      description = '`.github/prlint.json` not found'
+    }
+    const statusUrl = `https://api.github.com/repos/${body.repository.full_name}/statuses/${body.pull_request.head.sha}`
+    const status = await got.post(statusUrl, {
+      headers: {
+        Accept: "application/vnd.github.machine-man-preview+json",
+        Authorization: `token ${accessToken}`
+      },
+      body: {
+        state: 'error',
+        description: description,
+        context: 'PRLint',
+        target_url: 'https://github.com/ewolfe/prlint#support'
+      },
+      json: true
+    });
+    send(res, 500, exception);
+  }
+}
+
+// INITIALIZE APP VARIABLES
+const accessTokens = {};
+let JWT = newJsonWebToken();
+
+module.exports = async (req, res) => {
+  if (req.url === '/favicon.ico') {
+    res.writeHead(200, {'Content-Type': 'image/x-icon'} );
+    res.end();
+  }
+
+  if (req.url === '/') {
+    send(res, 200, accessTokens);
+  }
+
+  if (req.url === '/webhook' && req.method === 'POST') {
+    const body = await json(req);
+
+    if (body && body.installation && body.installation.id && accessTokens[`${body.installation.id}`]) {
+      await updateShaStatus(body, res);
+    } else if (body && body.installation && body.installation.id && !accessTokens[`${body.installation.id}`]) {
+      try {
+        const response = await got.post(`https://api.github.com/installations/${body.installation.id}/access_tokens`, {
+          headers: {
+            Accept: "application/vnd.github.machine-man-preview+json",
+            Authorization: `Bearer ${JWT}`
+          }
+        });
+        accessTokens[`${body.installation.id}`] = JSON.parse(response.body);
+        await updateShaStatus(body, res);
+      } catch (exception) {
+        send(res, 500, exception);
+      }
+    } else {
+      send(res, 400, `invalid request payload`);
+    }
+  }
+}
