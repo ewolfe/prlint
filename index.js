370c37ccb15e4bac87f550f3993f09000d62df30
ewolfe <e@ewolfe.me>
2017-12-15 08:52:18 -0800

    style: fix ESLint warnings and errors

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const flatten = require('flat');
-const fs = require('fs');
 const got = require('got');
 const jsonwebtoken = require('jsonwebtoken');
 const Raven = require('raven');
 const { json, send } = require('micro');
 
+const accessTokens = {};
+
 Raven.config('https://e84d90e8ec13450d924ddd1a19581c62:aa9224cf89544c0591bf839112161adf@sentry.io/251839', {
   autoBreadcrumbs: {
-    http: true
-  }
+    http: true,
+  },
 }).install();
 
 function newJsonWebToken() {
@@ -16,30 +17,30 @@ function newJsonWebToken() {
   const payload = {
     iat: Math.floor(Date.now() / 1000),
     exp: Math.floor(Date.now() / 1000) + (10 * 60),
-    iss: 7012 // https://github.com/settings/apps/prlint
-  }
+    iss: 7012, // https://github.com/settings/apps/prlint
+  };
 
-  let privateKeyBuffer = new Buffer(process.env.PRIVATE_KEY_B64, 'base64');
-  let privateKey = privateKeyBuffer.toString('ascii');
-  return jsonwebtoken.sign(payload, privateKey, { algorithm: 'RS256'});
+  const privateKeyBuffer = Buffer.from(process.env.PRIVATE_KEY_B64, 'base64');
+  const privateKey = privateKeyBuffer.toString('ascii');
+  return jsonwebtoken.sign(payload, privateKey, { algorithm: 'RS256' });
 }
 
 async function updateShaStatus(body, res) {
-  const pull_request_flat = flatten(body.pull_request);
+  const pullRequestFlat = flatten(body.pull_request);
   const accessToken = accessTokens[`${body.installation.id}`].token;
 
   try {
-    const configUrl = `https://api.github.com/repos/${body.repository.full_name}/contents/.github/prlint.json?ref=${body.pull_request.head.ref}`
+    const configUrl = `https://api.github.com/repos/${body.repository.full_name}/contents/.github/prlint.json?ref=${body.pull_request.head.ref}`;
     const config = await got(configUrl, {
       headers: {
-        Accept: "application/vnd.github.machine-man-preview+json",
-        Authorization: `token ${accessToken}`
-      }
+        Accept: 'application/vnd.github.machine-man-preview+json',
+        Authorization: `token ${accessToken}`,
+      },
     });
 
-    const userConfigBase64 = JSON.parse(config.body).content
+    const userConfigBase64 = JSON.parse(config.body).content;
 
-    let failureMessages = [];
+    const failureMessages = [];
     let userConfig;
 
     try {
@@ -48,102 +49,100 @@ async function updateShaStatus(body, res) {
       failureMessages.push(e);
     }
 
-    Object.keys(userConfig).map(element => {
+    Object.keys(userConfig).forEach((element) => {
       userConfig[element].forEach((item, index) => {
-        const pattern = item.pattern;
+        const { pattern } = item;
         const regex = new RegExp(pattern, item.flags || '');
-        pass = regex.test(pull_request_flat[element])
+        const pass = regex.test(pullRequestFlat[element]);
         if (!pass) {
           let message = `Rule \`${element}[${index}]\` failed`;
           message = item.message || message;
           failureMessages.push(message);
         }
-      })
-    })
+      });
+    });
 
     let bodyPayload = {};
     if (!failureMessages.length) {
       bodyPayload = {
         state: 'success',
         description: 'Your validation rules passed',
-        context: 'PRLint'
+        context: 'PRLint',
       };
     } else {
       let description = failureMessages[0];
       if (failureMessages.length > 1) {
-        description = `1/${failureMessages.length - 1}: ${description}`
+        description = `1/${failureMessages.length - 1}: ${description}`;
       }
       bodyPayload = {
         state: 'failure',
         description: description.slice(0, 140), // 140 characters is a GitHub limit
         target_url: `https://github.com/${body.repository.full_name}/blob/${body.pull_request.head.sha}/.github/prlint.json`,
-        context: 'PRLint'
-      }
+        context: 'PRLint',
+      };
     }
 
     try {
-      const statusUrl = `https://api.github.com/repos/${body.repository.full_name}/statuses/${body.pull_request.head.sha}`
-      const status = await got.post(statusUrl, {
+      const statusUrl = `https://api.github.com/repos/${body.repository.full_name}/statuses/${body.pull_request.head.sha}`;
+      await got.post(statusUrl, {
         headers: {
-          Accept: "application/vnd.github.machine-man-preview+json",
-          Authorization: `token ${accessToken}`
+          Accept: 'application/vnd.github.machine-man-preview+json',
+          Authorization: `token ${accessToken}`,
         },
         body: bodyPayload,
-        json: true
+        json: true,
       });
       send(res, 200, bodyPayload);
     } catch (exception) {
       Raven.captureException(exception);
       send(res, 500, {
-        exception: exception,
+        exception,
         request_body: bodyPayload,
-        response: exception.response.body
+        response: exception.response.body,
       });
     }
   } catch (exception) {
     let description = exception.toString();
     let statusCode = 200;
     if (exception.response && exception.response.statusCode === 404) {
-      description = '`.github/prlint.json` not found'
+      description = '`.github/prlint.json` not found';
     } else {
       statusCode = 500;
       Raven.captureException(exception);
     }
-    const statusUrl = `https://api.github.com/repos/${body.repository.full_name}/statuses/${body.pull_request.head.sha}`
-    const status = await got.post(statusUrl, {
+    const statusUrl = `https://api.github.com/repos/${body.repository.full_name}/statuses/${body.pull_request.head.sha}`;
+    await got.post(statusUrl, {
       headers: {
-        Accept: "application/vnd.github.machine-man-preview+json",
-        Authorization: `token ${accessToken}`
+        Accept: 'application/vnd.github.machine-man-preview+json',
+        Authorization: `token ${accessToken}`,
       },
       body: {
         state: 'error',
-        description: description,
+        description,
         context: 'PRLint',
-        target_url: 'https://github.com/ewolfe/prlint#support'
+        target_url: 'https://github.com/ewolfe/prlint#support',
       },
-      json: true
+      json: true,
     });
     send(res, statusCode, description);
   }
 }
 
-// INITIALIZE APP VARIABLES
-const accessTokens = {};
 let JWT = newJsonWebToken();
 
 // Refresh the JSON Web Token every X milliseconds
 setInterval(() => {
   JWT = newJsonWebToken();
-}, 300000 /* 5 minutes */)
+}, 300000 /* 5 minutes */);
 
 module.exports = async (req, res) => {
   if (req.url === '/favicon.ico') {
-    res.writeHead(200, {'Content-Type': 'image/x-icon'} );
+    res.writeHead(200, { 'Content-Type': 'image/x-icon' });
     res.end();
   }
 
   if (req.url === '/status' && req.method === 'GET') {
-    res.end('OK')
+    res.end('OK');
   }
 
   if (req.url === '/webhook' && req.method === 'POST') {
@@ -153,21 +152,21 @@ module.exports = async (req, res) => {
     } else if (body && body.action && body.action === 'closed') {
       send(res, 200, body);
     } else if (
-        body &&
-        body.pull_request &&
-        body.installation &&
-        body.installation.id &&
-        accessTokens[`${body.installation.id}`] &&
-        (new Date(accessTokens[`${body.installation.id}`].expires_at) > new Date()) // make sure token expires in the future
-      ) {
+      body &&
+      body.pull_request &&
+      body.installation &&
+      body.installation.id &&
+      accessTokens[`${body.installation.id}`] &&
+      (new Date(accessTokens[`${body.installation.id}`].expires_at) > new Date()) // make sure token expires in the future
+    ) {
       await updateShaStatus(body, res);
     } else if (body && body.pull_request && body.installation && body.installation.id) {
       try {
         const response = await got.post(`https://api.github.com/installations/${body.installation.id}/access_tokens`, {
           headers: {
-            Accept: "application/vnd.github.machine-man-preview+json",
-            Authorization: `Bearer ${JWT}`
-          }
+            Accept: 'application/vnd.github.machine-man-preview+json',
+            Authorization: `Bearer ${JWT}`,
+          },
         });
         accessTokens[`${body.installation.id}`] = JSON.parse(response.body);
         await updateShaStatus(body, res);
@@ -176,10 +175,10 @@ module.exports = async (req, res) => {
         send(res, 500, exception);
       }
     } else {
-      send(res, 400, `invalid request payload`);
+      send(res, 400, 'invalid request payload');
     }
   } else {
-    res.writeHead(301, {'Location': 'https://github.com/ewolfe/prlint'} );
+    res.writeHead(301, { Location: 'https://github.com/ewolfe/prlint' });
     res.end();
   }
-}
+};
