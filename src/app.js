be6724210187430f6f3e48782c1b4991bff46777
Hrusikesh Panda <hrusikesh.panda@dowjones.com>
2019-02-08 20:03:37 -0500

    Segregating web server from linter orchestration.

--- src/app.js
+++ src/app.js
@@ -3,40 +3,11 @@ const { json, send } = require('micro');
 const setupErrorReporting = require('./setupErrorReporting');
 
 const newJsonWebToken = require('./utils/newJsonWebToken.js');
-const linter = require('./linter.js');
-const GithubRepo = require('./github.js');
+const lintPullRequest = require('./linter.js');
 
 const Raven = setupErrorReporting();
 
 const accessTokens = {};
-
-async function updateShaStatus({ githubRepo, prlintDotJson, failureMessages, failureURLs, accessToken }) {
-  const bodyPayload = githubRepo.createBodyPayload({
-    failureMessages,
-    failureURLs,
-  });
-  try {
-    // Build up a status for sending to the pull request
-    // POST the status to the pull request
-    await githubRepo.postValidationStatus({
-      bodyPayload,
-      accessToken,
-    });
-
-    return { status: 200, payload: bodyPayload };
-  } catch (exception) {
-    Raven.captureException(exception, { extra: prlintDotJson });
-
-    return {
-      status: 500,
-      payload: {
-        exception,
-        request_body: bodyPayload,
-        response: exception.response.body,
-      },
-    };
-  }
-}
 
 // Get a JWT on server start
 let JWT = newJsonWebToken();
@@ -69,99 +40,21 @@ module.exports = async (req, res) => {
 
   // Used by GitHub
   if (req.url === '/webhook' && req.method === 'POST') {
-    const body = await json(req);
+    const payload = await json(req);
 
-    if (!body) {
-      // Doubtful GitHub will ever end up at this block
-      // but it was useful while I was developing
-      send(res, 400, 'invalid request payload');
-      return;
-    }
+    const accessToken = accessTokens[`${payload.installation.id}`];
 
-    if (!body.pull_request) {
-      // We just return the data that was sent to the webhook
-      // since there's not really anything for us to do in this situation
-      send(res, 200, body);
-      return;
-    }
+    const { status, exception, freshToken, body, extra } = lintPullRequest({ payload, accessToken, JWT });
 
-    if (body.action && body.action === 'closed') {
-      // No point in linting anything if the pull request is closed
-      send(res, 200, body);
-      return;
-    }
+    console.log({ status, exception, freshToken, body, extra });
 
-    if (!body.installation || !body.installation.id) {
-      // Doubtful GitHub will ever end up at this block
-      // but it was useful while I was developing
-      send(res, 400, 'missing installation id, see if the app was registered correctly');
-      return;
-    }
+    if (freshToken && accessToken.token !== freshToken.token) accessTokens[`${payload.installation.id}`] = freshToken;
 
-    const githubRepo = new GithubRepo(body);
-    let accessToken = accessTokens[`${body.installation.id}`];
-
-    try {
-      if (
-        !accessToken ||
-        !new Date(accessToken.expires_at) > new Date() // make sure token expires in the future
-      ) {
-        try {
-          // token has expired or not valid. in this case, get a new one
-          accessToken = await githubRepo.getAccessToken({ JWT });
-          accessTokens[`${body.installation.id}`] = accessToken;
-          accessToken = accessTokens[`${body.installation.id}`].token;
-        } catch (exception) {
-          Raven.captureException(exception);
-          send(res, 500, {
-            token: accessTokens[`${body.installation.id}`],
-            exception,
-          });
-        }
-      }
-      // Initialize variables
-      const failureMessages = [];
-      const failureURLs = [];
-
-      const { prlintDotJson, failureMessages: prlintFetchFailureMsgs } = await githubRepo.fetchPRLintJson({
-        accessToken,
-      });
-
-      failureMessages.push(...prlintFetchFailureMsgs);
-
-      // Run each of the validations (regex's)
-      if (prlintDotJson) {
-        const { failureMessages: linterFailureMsgs, failureURLs: linterFailureURLs } = linter({
-          prlintDotJson,
-          pullRequest: body.pull_request,
-        });
-        failureMessages.push(...linterFailureMsgs);
-        failureURLs.push(...linterFailureURLs);
-      }
-
-      const { status, payload } = await updateShaStatus({
-        githubRepo,
-        prlintDotJson,
-        failureMessages,
-        failureURLs,
-        accessToken,
-      });
-      send(res, status, payload);
-    } catch (exception) {
-      // If anyone of the "happy path" logic above failed
-      // then we post an update to the pull request that our
-      // application (PRLint) had issues, or that they're missing
-      // a configuration file (./.github/prlint.json)
-      let status = 200;
-      if (exception.response && exception.response.statusCode === 404) {
-        await githubRepo.post404Status({ accessToken });
-      } else {
-        status = 500;
-        Raven.captureException(exception);
-        await githubRepo.post500Status({ accessToken, exception });
-      }
-
-      send(res, status, exception.toString());
+    if (status !== 200) {
+      Raven.captureException(exception || body, extra);
+      send(res, status, (exception && exception.toString()) || body);
+    } else {
+      send(res, status, body);
     }
   } else {
     // Redirect since we don't need anyone visiting our service
--- src/linter.js
+++ src/linter.js
@@ -1,11 +1,13 @@
 const flatten = require('flat');
+const GithubRepo = require('./github.js');
+const newJsonWebToken = require('./utils/newJsonWebToken.js');
 
-module.exports = function ({ prlintDotJson = {}, pullRequest = {} }) {
-  const pullRequestFlattened = flatten(pullRequest);
+const validate = ({ prlintDotJson = {}, pullRrequest = {}, defaultFailureURL }) => {
+  const pullRequestFlattened = flatten(pullRrequest);
   const failureMessages = [];
   const failureURLs = [];
 
-  Object.keys(prlintDotJson).forEach((element) => {
+  Object.keys(prlintDotJson).forEach(element => {
     if (prlintDotJson[element]) {
       prlintDotJson[element].forEach((item, index) => {
         const { pattern } = item;
@@ -29,3 +31,138 @@ module.exports = function ({ prlintDotJson = {}, pullRequest = {} }) {
 
   return { failureMessages, failureURLs };
 };
+
+const shouldLint = body => {
+  if (!body) {
+    // Doubtful GitHub will ever end up at this block
+    // but it was useful while I was developing
+    return { proceed: false, status: 400, body: 'invalid request payload' };
+  }
+
+  if (!body.pull_request) {
+    // We just return the data that was sent to the webhook
+    // since there's not really anything for us to do in this situation
+    return { proceed: false, status: 200, body };
+  }
+
+  if (body.action && body.action === 'closed') {
+    // No point in linting anything if the pull request is closed
+    return { proceed: false, status: 200, body };
+  }
+
+  if (!body.installation || !body.installation.id) {
+    // Doubtful GitHub will ever end up at this block
+    // but it was useful while I was developing
+    return { proceed: false, status: 400, body: 'missing installation id, see if the app was registered correctly' };
+  }
+
+  return { proceed: true };
+};
+
+const orchestrate = async ({ payload, accessToken, JWT }) => {
+  // eslint-disable-next-line prefer-const
+  let { proceed, status, body } = shouldLint(payload);
+  console.log({ proceed, status, body });
+
+  if (!proceed) {
+    // let the caller return http response however they want
+    return { status, body };
+  }
+
+  status = 200;
+
+  const githubRepo = new GithubRepo(payload);
+  let freshToken = accessToken;
+
+  if (
+    !accessToken ||
+    !new Date(accessToken.expires_at) > new Date() // make sure token expires in the future
+  ) {
+    try {
+      // token has expired or not valid. in this case, get a new one
+      // use passed in JWT or generate our own
+      const accessTokenObj = await githubRepo.getAccessToken({ jsonWebToken: JWT || newJsonWebToken() });
+      freshToken = accessTokenObj;
+    } catch (exception) {
+      return { status: 500, exception, body: 'unable to authenticate with github' };
+      // Raven.captureException(exception);
+      // send(res, 500, {
+      //   token: newToken,
+      //   exception,
+      // });
+    }
+  }
+
+  let bodyPayload;
+  let prlintDotJson;
+
+  try {
+    // Initialize variables
+    const failureMessages = [];
+    const failureURLs = [];
+
+    const fetchPRLintResponse = await githubRepo.fetchPRLintJson({
+      accessToken: freshToken.token,
+    });
+
+    ({ prlintDotJson } = fetchPRLintResponse.prlintDotJson);
+    failureMessages.push(fetchPRLintResponse.failureMsgs);
+
+    // Run each of the validations (regex's)
+    if (prlintDotJson) {
+      const { failureMessages: linterFailureMsgs, failureURLs: linterFailureURLs } = validate({
+        prlintDotJson,
+        pullRequest: payload.pull_request,
+      });
+      failureMessages.push(...linterFailureMsgs);
+      failureURLs.push(...linterFailureURLs);
+    }
+
+    bodyPayload = githubRepo.createBodyPayload({
+      failureMessages,
+      failureURLs,
+    });
+  } catch (exception) {
+    // If anyone of the "happy path" logic above failed
+    // then we post an update to the pull request that our
+    // application (PRLint) had issues, or that they're missing
+    // a configuration file (./.github/prlint.json)
+    // status = 200;
+    if (exception.response && exception.response.statusCode === 404) {
+      status = 404;
+      await githubRepo.post404Status({ accessToken });
+    } else {
+      status = 500;
+      // Raven.captureException(exception);
+      await githubRepo.post500Status({ accessToken, exception, extra: { prlintDotJson } });
+    }
+
+    return { status, exception, freshToken };
+  }
+
+  try {
+    await githubRepo.postValidationStatus({
+      bodyPayload,
+      accessToken: freshToken.token,
+    });
+
+    return { status: 200, body: bodyPayload, freshToken };
+  } catch (exception) {
+    // If anyone of the "happy path" logic above failed
+    // then we post an update to the pull request that our
+    // application (PRLint) had issues,
+    await githubRepo.post500Status({ accessToken: freshToken.token, exception });
+    return {
+      status: 500,
+      exception,
+      extra: {
+        request_body: bodyPayload,
+        response: exception.response.body,
+        prlintDotJson,
+      },
+      freshToken,
+    };
+  }
+};
+
+module.exports = orchestrate;
