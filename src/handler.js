f4395ee938e419450a97e4c6ce53d5de69a7b124
Hrusikesh Panda <mrchief_2000@yahoo.com>
2019-02-10 22:41:33 -0500

    chore: Add final reporting logic back

--- src/handler.js
+++ src/handler.js
@@ -5,6 +5,43 @@ const { lint, defaultConfig } = require('./core');
 
 const APP_NAME = 'PRLint';
 
+const createFinalReport = ({
+  failureMessages,
+  failureURLs,
+  defaultFailureURL,
+}) => {
+  let bodyPayload = {};
+  if (!failureMessages.length) {
+    bodyPayload = {
+      state: 'success',
+      description: 'Your validation rules passed',
+    };
+  } else {
+    let description = failureMessages[0];
+    let URL = failureURLs[0];
+    if (failureMessages.length > 1) {
+      description = `1/${failureMessages.length - 1}: ${description}`;
+      URL = defaultFailureURL;
+    }
+    if (description) {
+      bodyPayload = {
+        state: 'failure',
+        description: description.slice(0, 140), // 140 characters is a GitHub limit
+        target_url: URL,
+      };
+    } else {
+      bodyPayload = {
+        state: 'failure',
+        description:
+          'Something went wrong with PRLint - You can help by opening an issue (click details)',
+        target_url: 'https://github.com/ewolfe/prlint/issues/new',
+      };
+    }
+  }
+
+  return bodyPayload;
+};
+
 module.exports = async (context) => {
   const { repos } = context.github;
   const { sha } = context.payload.pull_request.head;
@@ -24,8 +61,6 @@ module.exports = async (context) => {
       description: 'Waiting for the status to be reported',
     });
 
-    const report = { valid: true, failures: [] };
-
     // get prlint.json. get the default one in case of 404
     const prlintDotJson = await context.config('prlint.json', defaultConfig);
     const { failureMessages, failureURLs } = lint({
@@ -37,8 +72,7 @@ module.exports = async (context) => {
     // Final status
     await repos.createStatus({
       ...statusInfo,
-      state: report.valid ? 'success' : 'failure',
-      description: `found ${failureMessages} problems, ${failureURLs} warnings`,
+      ...createFinalReport({ failureMessages, failureURLs, defaultFailureURL }),
     });
   } catch (e) {
     log({
