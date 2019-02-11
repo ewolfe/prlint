bea08dcc64d88486f6f231d39e5067bae08ce762
Hrusikesh Panda <mrchief_2000@yahoo.com>
2019-02-10 21:51:59 -0500

    chore: Subscribe to every event but "closed"

--- src/app.js
+++ src/app.js
@@ -5,11 +5,19 @@ module.exports = (app) => {
   const router = app.route('/');
   configureRoutes(router);
 
-  // https://developer.github.com/webhooks/#events
-  // github exposes several pull_request events (https://developer.github.com/v3/activity/events/types/#pullrequestevent)
-  // but we're not interested in anything that potentially doesn't affect the title.
+  // https://developer.github.com/v3/activity/events/types/#pullrequestevent
+  // subscribe to every event but "closed"
   app.on(
-    ['pull_request.opened', 'pull_request.edited', 'pull_request.reopened'],
+    [
+      'pull_request.opened',
+      'pull_request.edited',
+      'pull_request.reopened',
+      'pull_request.assigned',
+      'pull_request.unassigned',
+      'pull_request.labeled',
+      'pull_request.unlabeled',
+      'pull_request.synchronized',
+    ],
     lintPullRequest,
   );
 };
